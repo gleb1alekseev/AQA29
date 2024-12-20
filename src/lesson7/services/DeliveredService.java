de892955aa263f35ef834223d9f1b6e36cd9ad16
g.alekseev <gleb1alekseev@gmail.com>
2024-12-20 22:20:25 +0300

    lesson 7 add default for DeliveredService

--- src/lesson7/services/DeliveredService.java
+++ src/lesson7/services/DeliveredService.java
@@ -1,8 +1,15 @@
 package lesson7.services;
 
+import lesson7.Impl.DeliveryServiceImpl;
 import lesson7.Order;
 
 public interface DeliveredService {
 
-    void deliveredService(Order order);
+    default void deliveredService(Order order) {
+        if (DeliveryServiceImpl.WINDOW == order.getDeliveryService()) {
+            System.out.println("Выдача заказа через окно");
+        } else {
+            System.out.println("Доставка заказа на дом");
+        }
+    }
 }
